023233ca10d849bdb3342bb1c8174c52bdb0ad0e
uyo-obong <uyoobonga@gmail.com>
2019-02-04 02:20:28 +0100

    fixing css issue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,18 +7,16 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title> Clinex Blog</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Styles -->
+    <link href="{{ asset('admins/usercss/bootstrap.css') }}" rel="stylesheet">
+    <link href="{{ asset('admins/usercss/blog-home.css') }}" rel="stylesheet">
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/blog-home.css') }}" rel="stylesheet">
     
 </head>
 <body>
